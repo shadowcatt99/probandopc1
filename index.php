d8c54811e7f916ba1cee13a49c49e9e5d73f19f3
shadowcatt99 <shadowcatt99@gmail.com>
2017-05-04 08:51:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,52 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+<html><head>
+  <title>probandopc1</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ </head>
+ <body>
+ <div align="center">
+  	<img align="center" src="images/cabeza.png" border="0">
+ </div>
+  <table align="center" width="500">
+ <tbody><tr>
+  <td width="150"><img src="images/lateral.png" border="0"></td>
+  <td width="800">
+  	 <table align="center" width="700">
+  	  <?php
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "b74ba3320e82ec";
+  $password = "2d194843";
+  $dbname = "ad_bb6ddb2f34daf52";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT * from servicios";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {?>
+      <tr>
+      <td><?php echo $row['codigo_servicios']?></td>
+      <td><?php echo $row['nombre_servicios']?></td>
+      <td><?php echo $row['descripcion_servicios']?></td>
+      <td><?php echo $row['imagen_servicios']?></td>
+      </tr>
+     <?php }
+  } else {
+            <td><?php echo $row['descripcion_servicio']?></td>
+      echo "0 results";
+  }
+  $conn->close();
+ ?> </table>
+ </td>
+  <td><img src="images/lateral.png"></td>
+ </tr>
+ </tbody></table>
+ <div align="center">
+  	<img align="center" src="images/base.png" border="0">
+ </div>
+ 
+ 
+</body></html>
